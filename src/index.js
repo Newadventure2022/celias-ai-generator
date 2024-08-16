b9e03f4371c280ea28c8feaa516db17fff3832f4
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2024-08-16 16:08:40 +0200

    Updated the html and css to make it responsive

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ function generateWords(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "o8145f06881d09401ad03t8b0f09e589";
-  let context = `Please respond politely and provide a concise encouraging message. Ensure that the message is positive, uplifting, and free from inappropriate language. Each message should be unique and not duplicate any previous responses. The message should include the word "${instructionsInput.value}". Thank you.`;
-  let prompt = `Generate a new encouraging message using the word ${instructionsInput.value} and sign the words with <br/> <strong> '~ SheCodes AI'</strong>, but please do not put '' around SheCodes AI`;
+  let context = `Please respond politely and provide a concise encouraging message. Ensure that the message is positive, uplifting, and free from inappropriate language. Each message should be unique and not duplicate or repeat any previous responses. The message should include the word "${instructionsInput.value}". Thank you.`;
+  let prompt = `Generate a new encouraging message using the word ${instructionsInput.value} and sign the words with an emoji and <br/> <strong> '~ SheCodes AI'</strong>, but please do not put '' around SheCodes AI`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating poem");
