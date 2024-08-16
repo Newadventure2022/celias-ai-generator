d25ea143825a64cc1e0a0cc4a6d3c77d76687c2f
Newadventure2022 <celias.efl.esl.classes@gmail.com>
2024-08-16 14:47:03 +0200

    Renames the application, added axios and updated the html and css

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
-function generateWords(event) {
-  event.preventDefault();
-
-  new Typewriter("#words-result", {
-    strings: "The Encouraging Words Will Go Here",
+function displayWords(response) {
+  console.log("words generated");
+  new Typewriter("#words", {
+    strings: response.data.answer,
     autoStart: true,
     cursor: "",
     delay: 20,
   });
 }
 
+function generateWords(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "o8145f06881d09401ad03t8b0f09e589";
+  let context = `Please respond politely and provide a concise encouraging message. Ensure that the message is positive, uplifting, and free from inappropriate language. Each message should be unique and not duplicate any previous responses. The message should include the word "${instructionsInput.value}". Thank you.`;
+  let prompt = `Generate a new encouraging message using the word ${instructionsInput.value} and sign the words with 'SheCodes AI'`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating poem");
+  console.log(`prompt: ${prompt}`);
+  console.log(`context: ${context}`);
+
+  axios.get(apiUrl).then(displayWords);
+}
 let wordsFormElement = document.querySelector("#words-form");
 wordsFormElement.addEventListener("submit", generateWords);
